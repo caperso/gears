08a82db8b35b38b4eb1f153684bbeb87e5a6e4b2
dimlights <qq174028@sina.com>
2020-02-07 12:35:31 +0800

    change dir

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,92 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { Icon, Layout, Menu } from 'antd';
+import React, { useState } from 'react';
+import { NavLink, Route, Switch, withRouter } from 'react-router-dom';
+import './App.scss';
+import { IRouteCfgProps, routeCfg } from './config';
+import { NotFound } from './pages/NotFound';
 
-const App = () => {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const { Header, Sider, Content } = Layout;
+const { SubMenu } = Menu;
+
+export function RouteWithSubRoutes(route: IRouteCfgProps) {
+    return (
+        <Route
+            path={route.path}
+            render={props => (
+                // pass the sub-routes down to keep nesting
+                <route.component {...props} routes={route.routes} />
+            )}
+        />
+    );
 }
 
-export default App;
+const App: React.FC = () => {
+    const [collapsed, setCollapsed] = useState(false);
+
+    function toggle() {
+        setCollapsed(!collapsed);
+    }
+
+    return (
+        <div className="App">
+            <Layout>
+                <Sider trigger={null} collapsible collapsed={collapsed}>
+                    <div className="logo" />
+                    <Menu theme="dark" mode="inline" defaultSelectedKeys={['home']} defaultOpenKeys={['comp']}>
+                        {routeCfg.map(routeItem => {
+                            if (routeItem.routes) {
+                                return (
+                                    <SubMenu
+                                        key={routeItem.key}
+                                        title={
+                                            <span>
+                                                {routeItem.icon && <Icon type={routeItem.icon} />}
+                                                <span>{routeItem.title}</span>
+                                            </span>
+                                        }
+                                    >
+                                        {routeItem.routes.map(subItem => {
+                                            return (
+                                                <Menu.Item key={subItem.key}>
+                                                    <NavLink to={subItem.path}>
+                                                        {subItem.icon && <Icon type={subItem.icon} />}
+                                                        <span>{subItem.title}</span>
+                                                    </NavLink>
+                                                </Menu.Item>
+                                            );
+                                        })}
+                                    </SubMenu>
+                                );
+                            } else if (routeItem.component) {
+                                return (
+                                    <Menu.Item key={routeItem.key}>
+                                        <NavLink to={routeItem.path}>
+                                            {routeItem.icon && <Icon type={routeItem.icon} />}
+                                            <span>{routeItem.title}</span>
+                                        </NavLink>
+                                    </Menu.Item>
+                                );
+                            }
+                            return null;
+                        })}
+                    </Menu>
+                </Sider>
+                <Layout>
+                    <Header style={{ background: '#fff', padding: 0 }}>
+                        <Icon className="trigger" type={collapsed ? 'menu-unfold' : 'menu-fold'} onClick={toggle} />
+                    </Header>
+                    <Content className="App-content">
+                        <Switch>
+                            {routeCfg.map(route => (
+                                <RouteWithSubRoutes key={route.key} {...route} />
+                            ))}
+                            <Route component={NotFound} />
+                        </Switch>
+                    </Content>
+                </Layout>
+            </Layout>
+        </div>
+    );
+};
+
+export default withRouter(App);
---
+++ src/pages/Home/index.tsx
@@ -0,0 +1,34 @@
+import { Avatar, Card, Col, Icon, Row, Statistic } from 'antd';
+import React from 'react';
+import img from './assets/panda.png';
+import './style.scss';
+
+const { Meta } = Card;
+
+const Home = () => {
+    const hour = new Date().getHours();
+    let hello = hour >= 12 && hour <= 17 ? '下午好' : hour >= 17 && hour <= 24 ? '晚上好' : '早上好';
+
+    return (
+        <div className="p-home">
+            <h3> 组件平台 </h3>
+            <Row>
+                <Col span={20}>
+                    <Meta
+                        avatar={<Avatar style={{ marginTop: 5 }} size="large" src={img} />}
+                        title={`${hello}，祝你开心每一天`}
+                        description="消除恐惧的最好办法就是面对恐惧！坚持，才是胜利。加油！奥利给！"
+                    />
+                </Col>
+                <Col style={{ marginTop: -13 }} span={4}>
+                    <Statistic title="当前组件总数" value={routeCfg[1].routes?.length} prefix={<Icon type="like" />} />
+                </Col>
+            </Row>
+            <div className="img-wrapper">
+                <img src={img} alt="组件平台" />
+            </div>
+        </div>
+    );
+};
+
+export default Home;
