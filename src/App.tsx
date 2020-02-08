4eeda2e8932e0bf056d29d8eae22d539581f486c
dimlights <qq174028@sina.com>
2020-02-08 12:28:05 +0800

    add router

--- src/App.tsx
+++ src/App.tsx
@@ -29,62 +29,60 @@ const App: React.FC = () => {
 
     return (
         <div className="App">
-            <Layout>
-                <Sider trigger={null} collapsible collapsed={collapsed}>
-                    <div className="logo" />
-                    <Menu theme="dark" mode="inline" defaultSelectedKeys={['home']} defaultOpenKeys={['comp']}>
-                        {routeCfg.map(routeItem => {
-                            if (routeItem.routes) {
-                                return (
-                                    <SubMenu
-                                        key={routeItem.key}
-                                        title={
-                                            <span>
-                                                {routeItem.icon && <Icon type={routeItem.icon} />}
-                                                <span>{routeItem.title}</span>
-                                            </span>
-                                        }
-                                    >
-                                        {routeItem.routes.map(subItem => {
-                                            return (
+                <Layout>
+                    <Sider trigger={null} collapsible collapsed={collapsed}>
+                        <div className="logo" />
+                        <Menu theme="dark" mode="inline" defaultSelectedKeys={['home']} defaultOpenKeys={['comp']}>
+                            {routeCfg.map(routeItem => {
+                                if (routeItem.routes) {
+                                    return (
+                                        <SubMenu
+                                            key={routeItem.key}
+                                            title={
+                                                <span>
+                                                    {routeItem.icon && <Icon type={routeItem.icon} />}
+                                                    <span>{routeItem.title}</span>
+                                                </span>
+                                            }
+                                        >
+                                            {routeItem.routes.map(subItem => (
                                                 <Menu.Item key={subItem.key}>
                                                     <NavLink to={subItem.path}>
                                                         {subItem.icon && <Icon type={subItem.icon} />}
                                                         <span>{subItem.title}</span>
                                                     </NavLink>
                                                 </Menu.Item>
-                                            );
-                                        })}
-                                    </SubMenu>
-                                );
-                            } else if (routeItem.component) {
-                                return (
-                                    <Menu.Item key={routeItem.key}>
-                                        <NavLink to={routeItem.path}>
-                                            {routeItem.icon && <Icon type={routeItem.icon} />}
-                                            <span>{routeItem.title}</span>
-                                        </NavLink>
-                                    </Menu.Item>
-                                );
-                            }
-                            return null;
-                        })}
-                    </Menu>
-                </Sider>
-                <Layout>
-                    <Header style={{ background: '#fff', padding: 0 }}>
-                        <Icon className="trigger" type={collapsed ? 'menu-unfold' : 'menu-fold'} onClick={toggle} />
-                    </Header>
-                    <Content className="App-content">
-                        <Switch>
-                            {routeCfg.map(route => (
-                                <RouteWithSubRoutes key={route.key} {...route} />
-                            ))}
-                            <Route component={NotFound} />
-                        </Switch>
-                    </Content>
+                                            ))}
+                                        </SubMenu>
+                                    );
+                                } else if (routeItem.component) {
+                                    return (
+                                        <Menu.Item key={routeItem.key}>
+                                            <NavLink to={routeItem.path}>
+                                                {routeItem.icon && <Icon type={routeItem.icon} />}
+                                                <span>{routeItem.title}</span>
+                                            </NavLink>
+                                        </Menu.Item>
+                                    );
+                                }
+                                return null;
+                            })}
+                        </Menu>
+                    </Sider>
+                    <Layout>
+                        <Header style={{ background: '#fff', padding: 0 }}>
+                            <Icon className="trigger" type={collapsed ? 'menu-unfold' : 'menu-fold'} onClick={toggle} />
+                        </Header>
+                        <Content className="App-content">
+                            <Switch>
+                                {routeCfg.map(route => (
+                                    <RouteWithSubRoutes key={route.key} {...route} />
+                                ))}
+                                <Route component={NotFound} />
+                            </Switch>
+                        </Content>
+                    </Layout>
                 </Layout>
-            </Layout>
         </div>
     );
 };
---
+++ src/components/image-preview/ImagePreview.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export const ImagePreview = () => {
+    return (
+        <div>
+            
+        </div>
+    )
+}
