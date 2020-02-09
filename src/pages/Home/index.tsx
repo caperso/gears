3554d21120783b92f5ab6573c7853333ba48d1d0
dimlights <qq174028@sina.com>
2020-02-09 15:38:02 +0800

    change text

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,4 +1,4 @@
-import { Avatar, Card, Col, Icon, Row, Statistic } from 'antd';
+import { Avatar, Card, Col, Row, Statistic } from 'antd';
 import { routeCfg } from 'config';
 import React from 'react';
 import img from '../../assets/image/panda.png';
@@ -17,12 +17,12 @@ const Home = () => {
                 <Col span={20}>
                     <Meta
                         avatar={<Avatar style={{ marginTop: 5 }} size="large" src={img} />}
-                        title={`${hello}，祝你开心每一天`}
-                        description="消除恐惧的最好办法就是面对恐惧！坚持，才是胜利。加油！奥利给！"
+                        title={`${hello}`}
+                        description="组件目录在侧栏, 请选择查看"
                     />
                 </Col>
                 <Col style={{ marginTop: -13 }} span={4}>
-                    <Statistic title="当前组件总数" value={routeCfg[1].routes?.length} prefix={<Icon type="like" />} />
+                    <Statistic title="当前组件总数" value={routeCfg[1].routes?.length} />
                 </Col>
             </Row>
         </div>
