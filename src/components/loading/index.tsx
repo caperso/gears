9071545933359cc00282da149a6ab4c6d59cf19c
caperso <qq174028@sina.com>
2020-03-03 13:14:51 +0800

    add new compoent loading

---
+++ src/components/loading/index.tsx
@@ -0,0 +1,8 @@
+import React from 'react'
+import './index.scss'
+
+
+export const Loading = (props: { handleClick: () => void }) => {
+    return <span className="g-icon-rotate" onClick={props.handleClick} />
+}
+
