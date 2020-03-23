42fc32310b6949ba82988001f47cd8f037757c6e
caperso <qq174028@sina.com>
2020-03-23 15:33:30 +0800

    remove: clean old

--- src/components/loading/index.tsx
+++
@@ -1,8 +0,0 @@
-import React from 'react'
-import './index.less'
-
-
-export const Loading = (props: { handleClick?: () => void }) => {
-    return <span className="g-icon-rotate" onClick={props.handleClick} />
-}
-
--- src/components/web-frame/index.tsx
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-import './index.less';
-
-export const WebFrame = (props: { url: string }) => {
-    const { url = '' } = props;
-    return <iframe src={url} className="g-frame" title='placeholder'></iframe>;
-};
