f27dbf0a98cc29276b18922cc8814c44bd40a016
caperso <qq174028@sina.com>
2020-04-02 17:53:28 +0800

    fix: styles update: hambutton

--- src/components/ham-button/demo/demo.tsx
+++ src/components/ham-button/demo/demo.tsx
@@ -1,6 +1,20 @@
-import React from 'react';
+import React, { useState } from 'react';
 import HamButton from '../HamButton';
+import './demo.less';
 
-export const HamButtonDemo = () => {
-  return <HamButton />;
+const HamButtonDemo = () => {
+  const [viewportWidth, setViewportWidth] = useState('100%');
+
+  const toggleViewportWidth = () => {
+    setViewportWidth(s => (s === '100%' ? '750px' : '100%'));
+  };
+
+  return (
+    <div className="demo-wrapper" style={{ width: viewportWidth }}>
+      <button onClick={toggleViewportWidth}>changeWidth</button>
+      <HamButton />
+    </div>
+  );
 };
+
+export default () => <HamButtonDemo />;
