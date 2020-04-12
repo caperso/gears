3111ca4b25ec3afdacd538367beb9426c5c05aab
caperso <qq174028@sina.com>
2020-04-12 17:47:49 +0800

    export gallery;

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
 export { default as SquareButton } from './components/developing/square-button';
 export { default as ContextMenu } from './components/maintaining/context-menu';
+export { default as Gallery } from './components/maintaining/gallery';
 export { default as ImagePreview } from './components/maintaining/image-preview';
 export { default as Levels } from './components/maintaining/levels';
